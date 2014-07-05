f1bdac9d4c1236e8ab2e835179f4ace2afdf801f
farukham <farukham@gmail.com>
2014-07-06 05:52:18 +0600

    First Commit

---
+++ js/custom.js
@@ -0,0 +1,21 @@
+jQuery(document).ready(function () {
+
+
+
+
+
+/*----------------------------------------------------*/
+/*  Animated Progress Bars
+/*----------------------------------------------------*/
+
+    jQuery('.skills li').each(function () {
+        jQuery(this).appear(function() {
+          jQuery(this).animate({opacity:1,left:"0px"},800);
+          var b = jQuery(this).find(".progress-bar").attr("data-width");
+          jQuery(this).find(".progress-bar").animate({
+            width: b + "%"
+          }, 1300, "linear");
+        }); 
+    });   
+
+});
